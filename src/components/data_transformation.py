b7aee0e26b8ba4859ae04bd95d2e6f89eedeab22
dhanesh sakre <dhaneshsakre@gmail.com>
2025-01-27 23:59:27 +0530

    data transformation2

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -54,3 +54,17 @@ class DataTransformation:
             return preprocessor
         except Exception as e:
             raise CustomException(e, sys)
+    
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info('reading of the train and test data is completed')
+            logging.info('obtaining preprocessor object')
+
+            preprocessor_obj = self.get_data_transformer_object()
+            target_column_name = "math_score"
+            numerical_columns = ["writing_score", "reading_score"]
+        except Exception as e:
+            raise CustomException(e, sys)
